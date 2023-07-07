8d6f4f80a570eb3dc2d8c29a1b5dc61df5ae4ffb
June <adasilva.html@gmail.com>
2023-07-07 11:04:44 -0300

    Try with browserrouter?

--- src/RouteSwitch.jsx
+++ src/RouteSwitch.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 export const RouteSwitch = () => {
   return (
-    <HashRouter basename={import.meta.env.DEV ? '/' : '/shopping-cart/'}>
+    <BrowserRouter basename={import.meta.env.DEV ? '/' : '/shopping-cart/'}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   );
 };
